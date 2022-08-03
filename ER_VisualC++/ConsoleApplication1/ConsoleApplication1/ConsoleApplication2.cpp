205f78404a4c24c44636977f53392a4ab524cdbf
alexcoronacion <alex.coronacion2022@gmail.com>
2022-08-03 16:17:54 -0500

    commit

--- ER_VisualC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication2.cpp
+++ ER_VisualC++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication2.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
-int main() {
-	cout << "hola";
+int main2() {
+	int num, digitoSig;
+	cout << "Ingrese un numero: "; cin >> num;
+	cout << "\n";
+	cout << "numero: " << num << "\n";
+	cout << "numero invertido: ";
+	do {
+		digitoSig = num % 10;
+		cout << digitoSig;
+		num /= 10;
+	} while (num > 0);
+	return 0;
 }
