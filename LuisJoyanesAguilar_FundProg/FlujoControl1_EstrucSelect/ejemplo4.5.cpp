c8488a4360d09b23765a596193492152d128fd32
alexcoronacion <alex.coronacion2022@gmail.com>
2023-05-12 23:43:56 -0500

    comit120523
    Falta crear este recibo

---
+++ LuisJoyanesAguilar_FundProg/FlujoControl1_EstrucSelect/ejemplo4.5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string nombre;
+    double horas,impuestos,sbruto,sneto;
+    cout<<"Ingresar nombre: ";
+    cin>>nombre;
+    cout<<"Ingresar horas: ";
+    cin>>horas;
+    cout<<"Ingresar tarifa: ";
+    return 0;
+}
